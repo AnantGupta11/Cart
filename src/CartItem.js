7e0553cbd4911d4661f9af7072b5ec031515bf68
Anant8002749 <ak8002749@gmail.com>
2020-06-11 13:31:44 +0530

    Adding state in cart item

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
         render(){
+            const { price, title, qty} = this.state;
             return(
                 <div className="cart-item">
                     <div className="left-block">
                         <img style={styles.image}></img>
                     </div>
                     <div className="right-block">
-                        <div style={{ fontSize:25 }}>Phone</div>
-                        <div style={{ color:'#777'}}>Rs. 999</div>
-                        <div style={{ color:'#777'}}>Qty: 1</div>
+                        <div style={{ fontSize:25 }}>{title}</div>
+                        <div style={{ color:'#777'}}>Rs. {price}</div>
+                        <div style={{ color:'#777'}}>Qty: {qty}</div>
                         <div className="cart-item-actions">
                             {/* Buttons */}
+                            <img alt="increase" className="action-icons" src="https://as1.ftcdn.net/jpg/03/46/94/28/500_F_346942841_2iGSPUeLvwj7JbgUjSewYiIkaJzjGJG9.jpg" />
+                            <img alt="decrese" className="action-icons" src="https://image.flaticon.com/icons/svg/659/659892.svg" />
+                            <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1345/1345874.svg" />
                         </div>
                     </div>
                 </div>
